d1b14f08e65cd7905346c8529487153475fbbe6a
lolamtisch <francisco.seipel@yahoo.de>
2019-08-04 11:05:47 +0200

    [TASK] Add paused handling

--- Examples/PassiveTab/main.js
+++ Examples/PassiveTab/main.js
@@ -8,26 +8,42 @@ chrome.runtime.sendMessage(extensionId, {mode: 'passive'}, function(response) {
 // Wait for presence Requests
 chrome.runtime.onMessage.addListener(function(info, sender, sendResponse) {
   console.log('Presence requested', info)
-  sendResponse(getPresence());
+  sendResponse(getPresence(info));
 });
 
-function getPresence(){
+function getPresence(info){
   function getMS(string) {
     const a = string.split(":")
     const seconds = string.split(":").length - 1 > 1  ? +a[0] * 3600 + +a[1] * 60 + +a[2] : +a[0] * 60 + +a[1]
     return seconds * 1000;
   }
   try{
-    return {
-      clientId: '607153108375830548',
-      presence: {
-        state: document.getElementsByClassName('playbackSoundBadge__titleContextContainer')[0].firstElementChild.textContent,
-        details: document.getElementsByClassName('playbackSoundBadge__titleLink')[0].lastElementChild.textContent,
-        startTimestamp: Date.now() - getMS(document.getElementsByClassName('playbackTimeline__timePassed')[0].lastElementChild.textContent),
-        largeImageKey: "soundcloud",
-        instance: true,
-      }
-    };
+
+    if(document.getElementsByClassName('playControl')[0].classList.contains('playing')){
+      return {
+        clientId: '607153108375830548',
+        presence: {
+          state: document.getElementsByClassName('playbackSoundBadge__titleContextContainer')[0].firstElementChild.textContent,
+          details: document.getElementsByClassName('playbackSoundBadge__titleLink')[0].lastElementChild.textContent,
+          startTimestamp: Date.now() - getMS(document.getElementsByClassName('playbackTimeline__timePassed')[0].lastElementChild.textContent),
+          largeImageKey: "soundcloud",
+          instance: true,
+        }
+      };
+    }else if(info.active){
+      return {
+        clientId: '607153108375830548',
+        presence: {
+          state: document.getElementsByClassName('playbackSoundBadge__titleContextContainer')[0].firstElementChild.textContent,
+          details: 'Paused',
+          largeImageKey: "soundcloud",
+          instance: true,
+        }
+      };
+    }else{
+      return {};
+    }
+
   }catch(e){
     console.error(e);
     return {};
