14cf05c73b9e9b8dcbbeef46466c50a379a18675
lolamtisch <francisco.seipel@yahoo.de>
2019-08-09 14:21:32 +0200

    [TASK] Add startup bat for now

---
+++ Server/startup_windows.bat
@@ -0,0 +1,39 @@
+:begin
+cls
+@echo off
+if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
+
+Set keyname=MAL Sync Program
+
+echo	MAL-Sync
+echo    1. Add MAL-Sync Program to Startup
+echo    2. Remove MAL-Sync Program From Startup
+echo    x. Exit
+
+set /p choice=  Choose A Service:
+if not '%choice%'== set %choice%=choice:~0,1%
+
+if '%choice%'=='1' goto :addstartup
+if '%choice%'=='2' goto :delstartup
+if '%choice%'=='x' goto :exit
+
+:addstartup
+cls
+
+if exist %~dp0server-win-output.exe (
+    reg add HKLM\Software\Microsoft\Windows\CurrentVersion\Run\ /v "%keyname%" /t REG_SZ /d %~dp0server-win-output.exe
+) else (
+    echo File not found!
+)
+
+timeout /t 2 >nul
+
+goto begin
+
+:delstartup
+cls
+reg delete HKLM\Software\Microsoft\Windows\CurrentVersion\Run\ /v "%keyname%" /f
+
+timeout /t 2 >nul
+
+goto begin
