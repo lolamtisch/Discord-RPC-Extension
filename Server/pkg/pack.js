f368358498ee5b6d6f23da8860ba0a825fdd2492
lolamtisch <francisco.seipel@yahoo.de>
2019-08-12 11:22:38 +0200

    [TASK] Pack linux and mac

--- Server/pkg/pack.js
+++ Server/pkg/pack.js
@@ -7,7 +7,6 @@ const dist = path.join(__dirname, '../../dist');
 var archive = archiver('zip', {
   zlib: { level: 9 }
 });
-
 archive.pipe(fs.createWriteStream(path.join(dist, '/windows_64bit.zip')));
 archive.file(path.join(dist, 'server_win_64bit.exe'), { name: 'server_win.exe' });
 archive.file(path.join(__dirname, 'startup_windows.bat'), { name: 'startup_windows.bat' });
@@ -16,8 +15,43 @@ archive.finalize();
 archive = archiver('zip', {
   zlib: { level: 9 }
 });
-
 archive.pipe(fs.createWriteStream(path.join(dist, '/windows_32bit.zip')));
 archive.file(path.join(dist, 'server_win_32bit.exe'), { name: 'server_win.exe' });
 archive.file(path.join(__dirname, 'startup_windows.bat'), { name: 'startup_windows.bat' });
+archive.finalize();
+
+archive = archiver('zip', {
+  zlib: { level: 9 }
+});
+archive.pipe(fs.createWriteStream(path.join(dist, '/windows_32bit_debug.zip')));
+archive.file(path.join(dist, 'server_win_32bit_debug.exe'), { name: 'server_win_debug.exe' });
+archive.file(path.join(__dirname, 'startup_windows.bat'), { name: 'startup_windows.bat' });
+archive.finalize();
+
+archive = archiver('zip', {
+  zlib: { level: 9 }
+});
+archive.pipe(fs.createWriteStream(path.join(dist, '/linux.zip')));
+archive.file(path.join(dist, 'server_linux_debug'), { name: 'server_linux_debug' });
+archive.finalize();
+
+archive = archiver('zip', {
+  zlib: { level: 9 }
+});
+archive.pipe(fs.createWriteStream(path.join(dist, '/linux_no_tray.zip')));
+archive.file(path.join(dist, 'server_linux_no_tray_debug'), { name: 'server_linux_no_tray_debug' });
+archive.finalize();
+
+archive = archiver('zip', {
+  zlib: { level: 9 }
+});
+archive.pipe(fs.createWriteStream(path.join(dist, '/macos.zip')));
+archive.file(path.join(dist, 'server_macos_debug'), { name: 'server_macos_debug' });
+archive.finalize();
+
+archive = archiver('zip', {
+  zlib: { level: 9 }
+});
+archive.pipe(fs.createWriteStream(path.join(dist, '/macos_no_tray.zip')));
+archive.file(path.join(dist, 'server_macos_no_tray_debug'), { name: 'server_macos_no_tray_debug' });
 archive.finalize();
