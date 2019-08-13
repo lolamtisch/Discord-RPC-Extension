8a00db46a4c2d5266443e774bdebf27c1774812f
lolamtisch <francisco.seipel@yahoo.de>
2019-08-13 10:48:11 +0200

    [TASK] Update chrome ID

--- Examples/ActiveTab/main.js
+++ Examples/ActiveTab/main.js
@@ -1,4 +1,4 @@
-var extensionId = "londahcleefkodmnlammpkcdjekmmafj"; //Chrome
+var extensionId = "agnaejlkbiiggajjmnpmeheigkflbnoo"; //Chrome
 if(typeof browser !== 'undefined' && typeof chrome !== "undefined"){
   extensionId = "1cf038ecb5fc74f2a6ca0811c2c14d89fc230b89@temporary-addon"; //Firefox
 }
