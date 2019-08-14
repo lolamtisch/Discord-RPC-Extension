69ac5961b483ec0b807c16cdb239f9f0fe11a1a8
lolamtisch <francisco.seipel@yahoo.de>
2019-08-14 13:29:59 +0200

    [TASK] Add firefox id

--- Examples/ActiveTab/main.js
+++ Examples/ActiveTab/main.js
@@ -1,6 +1,6 @@
 var extensionId = "agnaejlkbiiggajjmnpmeheigkflbnoo"; //Chrome
 if(typeof browser !== 'undefined' && typeof chrome !== "undefined"){
-  extensionId = "1cf038ecb5fc74f2a6ca0811c2c14d89fc230b89@temporary-addon"; //Firefox
+  extensionId = "{57081fef-67b4-482f-bcb0-69296e63ec4f}"; //Firefox
 }
 
 // Register Presence
