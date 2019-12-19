1269dd4281e0c237e6f571a44971cacf344af3c1
lolamtisch <francisco.seipel@yahoo.de>
2019-12-19 10:52:52 +0100

    [BUGFIX] Fix extension id

--- Examples/Party/background.js
+++ Examples/Party/background.js
@@ -1,4 +1,4 @@
-var extensionId = "ibpbmghbpjpjookgdkehfmfhnapijmeh"; //Chrome
+var extensionId = "agnaejlkbiiggajjmnpmeheigkflbnoo"; //Chrome
 if(typeof browser !== 'undefined' && typeof chrome !== "undefined"){
   extensionId = "{57081fef-67b4-482f-bcb0-69296e63ec4f}"; //Firefox
 }
