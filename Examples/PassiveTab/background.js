7d5a3a1b1e00e3eb29d97a0104c56ab4ba065dc7
lolamtisch <francisco.seipel@yahoo.de>
2019-12-23 15:21:12 +0100

    [TASK] Check request type

--- Examples/PassiveTab/background.js
+++ Examples/PassiveTab/background.js
@@ -1,6 +1,8 @@
 chrome.runtime.onMessageExternal.addListener(function(request, sender, sendResponse) {
-  chrome.tabs.sendMessage(request.tab, request.info, function(response){
-    sendResponse(response);
-  });
+  if(request.action == "presence") {
+    chrome.tabs.sendMessage(request.tab, request.info, function(response){
+      sendResponse(response);
+    });
+  }
   return true;
 });
