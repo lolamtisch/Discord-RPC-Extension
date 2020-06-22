5079b6b6af738091a466d2b3baa60c57bf29dbcd
MrBartusek <bartusekcraft@gmail.com>
2020-06-22 16:23:46 +0200

    Tweek startup script

--- Server/pkg/startup_windows.bat
+++ Server/pkg/startup_windows.bat
@@ -1,39 +1,35 @@
+@echo off
+
 :begin
 cls
-@echo off
 if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
 
-Set keyname=MAL Sync Program
+Set keyname=Discord RPC Extension
 
-echo	MAL-Sync
-echo    1. Add MAL-Sync Program to Startup
-echo    2. Remove MAL-Sync Program From Startup
-echo    x. Exit
+echo	Discord-RPC-Extension
+echo    1. Add Discord-RPC-Extensionc Program to Startup
+echo    2. Remove Discord-RPC-Extension Program From Startup
+echo    3. Exit
 
 set /p choice=  Choose A Service:
 if not '%choice%'== set %choice%=choice:~0,1%
 
 if '%choice%'=='1' goto :addstartup
 if '%choice%'=='2' goto :delstartup
-if '%choice%'=='x' goto :exit
+if '%choice%'=='3' goto :exit
 
 :addstartup
 cls
-
 if exist "%~dp0server_win.exe" (
     reg add HKLM\Software\Microsoft\Windows\CurrentVersion\Run\ /v "%keyname%" /t REG_SZ /d "%~dp0server_win.exe"
 ) else (
-    echo File not found!
+    echo Server file not found!
 )
-
 timeout /t 2 >nul
-
 goto begin
 
 :delstartup
 cls
 reg delete HKLM\Software\Microsoft\Windows\CurrentVersion\Run\ /v "%keyname%" /f
-
 timeout /t 2 >nul
-
 goto begin
