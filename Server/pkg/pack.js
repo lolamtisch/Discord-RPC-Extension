add0678e22958cddae507122da8be615a5114fc6
lolamtisch <francisco.seipel@yahoo.de>
2021-02-26 16:40:51 +0100

    [TASK] Add macos start script #30

--- Server/pkg/pack.js
+++ Server/pkg/pack.js
@@ -47,11 +47,13 @@ archive = archiver('zip', {
 });
 archive.pipe(fs.createWriteStream(path.join(dist, '/macos.zip')));
 archive.file(path.join(dist, 'server_macos_debug'), { name: 'server_macos_debug' });
+archive.file(path.join(__dirname, 'startup_macos.sh'), { name: 'start.sh' });
 archive.finalize();
 
 archive = archiver('zip', {
   zlib: { level: 9 }
 });
 archive.pipe(fs.createWriteStream(path.join(dist, '/macos_no_tray.zip')));
-archive.file(path.join(dist, 'server_macos_no_tray_debug'), { name: 'server_macos_no_tray_debug' });
+archive.file(path.join(dist, 'server_macos_no_tray_debug'), { name: 'server_macos_debug' });
+archive.file(path.join(__dirname, 'startup_macos.sh'), { name: 'start.sh' });
 archive.finalize();
---
+++ Server/pkg/startup_macos.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+cd "`dirname "$0"`"
+nohup ./server_macos_debug &
