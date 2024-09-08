ae2914292d08f2865d03ecdaec263a187b66fd8b
lolamtisch <12599334+lolamtisch@users.noreply.github.com>
2024-09-08 11:21:14 +0200

    [TASK] Drop win 32bit

--- Server/pkg/pack.js
+++ Server/pkg/pack.js
@@ -15,16 +15,8 @@ archive.finalize();
 archive = archiver('zip', {
   zlib: { level: 9 }
 });
-archive.pipe(fs.createWriteStream(path.join(dist, '/windows_32bit.zip')));
-archive.file(path.join(dist, 'server_win_32bit.exe'), { name: 'server_win.exe' });
-archive.file(path.join(__dirname, 'startup_windows.bat'), { name: 'startup_windows.bat' });
-archive.finalize();
-
-archive = archiver('zip', {
-  zlib: { level: 9 }
-});
-archive.pipe(fs.createWriteStream(path.join(dist, '/windows_32bit_debug.zip')));
-archive.file(path.join(dist, 'server_win_32bit_debug.exe'), { name: 'server_win_debug.exe' });
+archive.pipe(fs.createWriteStream(path.join(dist, '/windows_64bit_debug.zip')));
+archive.file(path.join(dist, 'server_win_64bit_debug.exe'), { name: 'server_win_debug.exe' });
 archive.file(path.join(__dirname, 'startup_windows.bat'), { name: 'startup_windows.bat' });
 archive.finalize();
 
